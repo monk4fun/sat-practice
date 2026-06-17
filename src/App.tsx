2fdaa7c7662b36bca77c664b6035983c726b0d6e
sansat <tran.n.anh@gmail.com>
2026-06-17 15:42:04 -0400

    Switch to HashRouter for guaranteed routing support

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { AppShell } from './components/layout/AppShell';
 import { HomePage } from './pages/HomePage';
 import { DrillPage } from './pages/DrillPage';
